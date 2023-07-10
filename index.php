a04e461d457e3024d469f0a6262775e0ed9f99a0
Joel <ndohjoelmbj16@gmail.com>
2023-07-10 12:42:28 +0100

    second commit

--- index.php
+++ index.php
@@ -53,7 +53,7 @@ if (isset($_POST['submit'])) {
 }
 ?>
 
-    <img src="/php-crash/feedback/img/logo.png" class="w-25 mb-3" alt="">
+    <img src="/img/logo.png" class="w-25 mb-3" alt="">
     <h2>Feedback</h2>
     <?php echo isset($name) ? $name : ''; ?>
     <p class="lead text-center">Leave feedback for Lavent Living</p>
